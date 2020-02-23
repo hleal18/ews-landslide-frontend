3b138903981618249c5242ebe3c5447484ac7f13
Humberto Leal <humberto.adolfo@hotmail.com>
2020-02-23 15:02:51 -0500

    Button with plus sign used for add new elements is created

---
+++ src/Presentational/AddButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import Icon from '@material-ui/core/Icon';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    // '& > span': {
+    //   margin: theme.spacing(2),
+    // },
+  },
+}));
+
+export default function Icons() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {/* <Icon color="primary" style={{fontSize: 50 }}>add_circle</Icon> */}
+      <IconButton color="primary" >
+          <AddCircleIcon style={{ fontSize: 100 }}/>
+      </IconButton>
+    </div>
+  );
+}
