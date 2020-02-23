ee57e10a5781e76034b82f0709d5f01928f6d7f6
Humberto Leal <humberto.adolfo@hotmail.com>
2020-02-23 15:03:34 -0500

    Add button component used in CriticalPointCards component

--- src/Presentational/Info/CriticalPointCards.js
+++ src/Presentational/Info/CriticalPointCards.js
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import CriticalPointCard from "./CriticalPointCard";
 import BreadCrumbs from './BreadCrumbs';
 import { withRouter } from 'react-router-dom';
+import AddButton from '../AddButton'
 
 const renderInfoComponent = ({ind, history}) => (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={ind}>
@@ -16,8 +17,11 @@ const CriticalPointCards = ({ criticalPoints, history }) => (
         <Container maxWidth={false}>
             <Grid container spacing ={3}direction="column" >
                 <Grid item xs={12}> <BreadCrumbs /></Grid>
-                <Grid container  spacing={3} direction="row" justify="center">
+                <Grid container  spacing={3} direction="row" justify="flex-start">
                     {criticalPoints.map((el, ind) => (renderInfoComponent({ind, history})))}
+                    <Grid container item xs={12} sm={6} md={4} lg={3} xl={2} alignItems="center" justify="center">
+                        <AddButton />
+                    </Grid>                    
                 </Grid>
             </Grid>
         </Container>
