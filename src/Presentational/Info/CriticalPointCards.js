2b09eb3f89cb64689ce4025241e2079b416f3139
Humberto Leal <humberto.adolfo@hotmail.com>
2020-02-23 15:49:44 -0500

    CriticalPointCards with BreadCrumbs and refactoring

--- src/Presentational/Info/CriticalPointCards.js
+++ src/Presentational/Info/CriticalPointCards.js
@@ -6,8 +6,8 @@ import BreadCrumbs from './BreadCrumbs';
 import { withRouter } from 'react-router-dom';
 import AddButton from '../AddButton'
 
-const renderInfoComponent = ({ind, history}) => (
-    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={ind}>
+const InfoComponent = ({ history }) => (
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} >
         <CriticalPointCard redirect={() => history.push('/devices')}/>
     </Grid>
 );
@@ -15,10 +15,19 @@ const renderInfoComponent = ({ind, history}) => (
 const CriticalPointCards = ({ criticalPoints, history }) => (
     <div>
         <Container maxWidth={false}>
-            <Grid container spacing ={3}direction="column" >
-                <Grid item xs={12}> <BreadCrumbs /></Grid>
+            <Grid container spacing={3} direction="column" >
+                <Grid item > 
+                    <BreadCrumbs 
+                        routes={['dashboard']}
+                        currentContent="Puntos Críticos"
+                        contents={['Dashboard']}
+                    />
+                </Grid>
                 <Grid container  spacing={3} direction="row" justify="flex-start">
-                    {criticalPoints.map((el, ind) => (renderInfoComponent({ind, history})))}
+                    {
+                        criticalPoints.map((el, ind) => 
+                            <InfoComponent key={ind} history={history} />)
+                    }
                     <Grid container item xs={12} sm={6} md={4} lg={3} xl={2} alignItems="center" justify="center">
                         <AddButton />
                     </Grid>                    
