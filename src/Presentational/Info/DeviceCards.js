64c69e25dfb72512f440432eceb864fea54143d8
Humberto Leal <humberto.adolfo@hotmail.com>
2020-02-23 15:04:03 -0500

    Add button component rendered into DeviceCards component

--- src/Presentational/Info/DeviceCards.js
+++ src/Presentational/Info/DeviceCards.js
@@ -3,6 +3,7 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import DeviceCard from "./DeviceCard";
 import BreadCrumbs from "./BreadCrumbs";
+import AddButton from "../AddButton";
 
 const renderInfoComponent = ({ind}) => (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2}  key={ind}>
@@ -14,9 +15,14 @@ export default ({ devices }) => (
     <div>
         <Container maxWidth={false}>
             <Grid container spacing={3} direction="row">
-                <Grid item xs={12}><BreadCrumbs/></Grid>
-                <Grid container spacing={3} direction="row" justify="center">
+                <Grid container direction="row">
+                    <Grid item xs={12} ><BreadCrumbs /></Grid>                    
+                </Grid>                
+                <Grid container spacing={3} direction="row" justify="flex-start">
                     {devices.map((el, ind) => (renderInfoComponent({ind})))}
+                    <Grid container item xs={12} sm={6} md={4} lg={3} xl={2} alignItems="center" justify="center">
+                        <AddButton />
+                    </Grid>
                 </Grid>
             </Grid>
         </Container>
