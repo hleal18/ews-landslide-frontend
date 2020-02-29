394d7b0c50f408ffc2c29f9c7fe847fd6c3e5d5f
Humberto Leal <humberto.adolfo@hotmail.com>
2020-02-29 18:10:40 -0500

    Select component used in dashboard filters

--- src/Presentational/Forms/SelectForm.js
+++ src/Presentational/Forms/SelectForm.js
@@ -1,35 +1,20 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
-import ListItemText from '@material-ui/core/ListItemText';
 import Select from '@material-ui/core/Select';
-import Checkbox from '@material-ui/core/Checkbox';
-import Chip from '@material-ui/core/Chip';
 
 const useStyles = makeStyles(theme => ({
     formControl: {
         margin: theme.spacing(1),
         minWidth: '50%',
         maxWidth: '100%',
-      },
-      chips: {
-        display: 'flex',
-        flexWrap: 'wrap',
-      },
-      chip: {
-        margin: 2,
-      },
-      noLabel: {
-        marginTop: theme.spacing(3),
-      },
+      }
 }));
 
-export default function SelectForm({ 
-    id, 
+export default function SelectForm({
+    id = "default",
     label = "default", 
     names = [], 
     value = "", 
@@ -40,21 +25,20 @@ export default function SelectForm({
     
     return (
         <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="age-native-simple">{label}</InputLabel>
+            <InputLabel >{label}</InputLabel>
             <Select
-              native
               value={value}
-              onChange={handleChange}
-              inputProps={{
-                  name: 'age',
-                  id: 'age-native-simple'
-              }}
+              onChange={(e) => {e.target.id = id; handleChange(e);}}
+              id={id}
+              name={label}
             >
-              {names.map((name, ind) => (
-                <option key={name} value={options[ind]} >
-                  {name}
-                </option>
-                ))}
+                {
+                    names.map((name, ind) => (
+                        <MenuItem key={name} value={options[ind]} >
+                            {name}
+                        </MenuItem>
+                        ))
+                }
             </Select>
         </FormControl>              
     )
