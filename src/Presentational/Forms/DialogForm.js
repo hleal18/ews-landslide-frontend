9b92bae9cb9045ffd15b5cb91a0fa4aba0a93f39
Humberto Leal <humberto.adolfo@hotmail.com>
2020-03-16 10:32:09 -0500

    Deleted unnecessary dependencies

--- src/Presentational/Forms/DialogForm.js
+++ src/Presentational/Forms/DialogForm.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Typography from '@material-ui/core/Typography';
 import Slide from '@material-ui/core/Slide';
-import Map from '../Info/Map/Map';
 
 // Transition for when the dialog appears, sliding up.
 const Transition = React.forwardRef(function Transition(props, ref) {
