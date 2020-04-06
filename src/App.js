59b708f18128765d73450255cd0f0cf623692571
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-06 16:08:30 -0500

    App component with dynamic routes for each entity in the system

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
 } from 'react-router-dom';
 
 import Grid from '@material-ui/core/Grid';
@@ -14,71 +15,99 @@ import DeviceCards from './Presentational/Info/DeviceCards';
 import Dashboard from './Dashboard2';
 import DashboardFilters from './Container/DashboardFiltersBars';
 import { grey } from '@material-ui/core/colors';
+import RiskZonesCardsManager from './Container/RiskZoneCardsManager';
 import CriticalPointCardsManager from './Container/CriticalPointCardsManager';
 import DeviceCardsManager from './Container/DeviceCardsManager';
 import VariableCardsManager from './Container/VariableCardsManager';
+import ConditionalLogin from './ConditionalLogin';
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/dashboard">
-            <Grid container direction="column">
-                <Grid item xs={12}>
-                    <AppBar />
-                <Grid item xs={12}>
-                    <DashboardFilters/>
-                </Grid>
-                </Grid>
-                <Grid item xs color={grey[900]}>
-                    <Dashboard />
-                </Grid>
-            </Grid>
-        </Route>
-        <Route path="/critical_points">
-            <Grid container direction="column">
-                <Grid item xs>
-                    <AppBar />
-                </Grid>
-                <Grid item xs>
-                    <CriticalPointCardsManager />
-                </Grid>
-            </Grid>
-        </Route>
-        <Route path="/devices">
-            <Grid container direction="column">
-                <Grid item xs>
-                    <AppBar />
-                </Grid>
-                <Grid item xs>
-                    <DeviceCardsManager />
-                </Grid>
-            </Grid>
-        </Route>
-        <Route path="/variables">
-            <Grid container direction="column">
-                <Grid item xs>
-                    <AppBar />
-                </Grid>
-                <Grid item xs>
-                    <VariableCardsManager />
-                </Grid>
-            </Grid>
-        </Route>
-        <Route path="/login" exact>
-          <SignIn />
-        </Route>
-        <Route path="/signup" exact>
-          <SignUp />
-        </Route>
-        <Route path="/">
-          <SignIn />
-        </Route>
-        
-      </Switch>
+// Contexts
+import { AuthProvider, AuthConsumer, useAuthenticated } from './Contexts/AuthContext';
+import { UserProvider, UserConsumer } from './Contexts/UserContext';
+import { RiskZonesProvider, RiskZonesConsumer } from './Contexts/RiskZonesContext';
 
-    </Router>
-  );
+function App() {    
+    return (
+        <Router>
+            <Switch>
+                <AuthProvider >
+                    <Route path="/dashboard">
+                        <Grid container direction="column">
+                            <Grid item xs={12}>
+                                <AppBar />
+                                <Grid item xs={12}>
+                                    <DashboardFilters />
+                                </Grid>
+                            </Grid>
+                            <Grid item xs color={grey[900]}>
+                                <Dashboard />
+                            </Grid>
+                        </Grid>
+                    </Route>
+                    <Route path="/riskzones/:riskZoneId/criticalspots/:criticalSpotId/sensornodes/:sensorNodeId/variables">
+                        <Grid container direction="column">
+                            <Grid item xs>
+                                <AppBar />
+                            </Grid>
+                            <Grid item xs>
+                                <VariableCardsManager />
+                            </Grid>
+                        </Grid>
+                    </Route>
+                    <Route path="/riskzones/:riskZoneId/criticalspots/:criticalSpotId/sensornodes">
+                        <Grid container direction="column">
+                            <Grid item xs>
+                                <AppBar />
+                            </Grid>
+                            <Grid item xs>
+                                <DeviceCardsManager />
+                            </Grid>
+                        </Grid>
+                    </Route>
+                    <Route path="/riskzones/:riskZoneId/criticalspots">
+                        <Grid container direction="column">
+                            <Grid item xs>
+                                <AppBar />
+                            </Grid>
+                            <Grid item xs>
+                                <RiskZonesProvider>
+                                    <RiskZonesConsumer>
+                                        {
+                                            (context) => <CriticalPointCardsManager context={context} />
+                                        }
+                                    </RiskZonesConsumer>
+                                </RiskZonesProvider>
+                            </Grid>
+                        </Grid>
+                    </Route>
+                    <Route path="/riskzones">
+                        <Grid container direction="column">
+                            <Grid item xs>
+                                <AppBar />
+                            </Grid>
+                            <Grid item xs>
+                                <RiskZonesProvider>
+                                    <RiskZonesCardsManager />
+                                </RiskZonesProvider>
+                            </Grid>
+                        </Grid>
+                    </Route>
+                    
+                    
+                    
+                    <Route path="/login" exact>
+                        <ConditionalLogin />
+                    </Route>
+                    <Route path="/signup" exact>
+                        <SignUp />
+                    </Route>
+                    <Route path="/" exact>
+                        <ConditionalLogin />
+                    </Route>
+                </AuthProvider>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
