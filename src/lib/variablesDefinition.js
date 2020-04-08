764b3e9e238f76cf5846926d70d1ba7100bdb378
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-08 17:47:47 -0500

    Central definition of variables supported in the app

---
+++ src/lib/variablesDefinition.js
@@ -0,0 +1,17 @@
+/**
+ * It contains definition of variables measured by the application.
+ * The definitions or values, are categorized as values used at
+ * system, application or back-end level and those used to be shown
+ * to the user, in this case, translated to spanish.
+ * This file is intended to be used as a centralized location
+ * where all the supported variables and its respective translations
+ * are stored. From here, different components can imported and execute
+ * appropriate functionality and show appropriate menus.
+ */
+
+export default {
+    soilMoisture: 'Humedad de Suelo',
+    inclination: 'Inclinación',
+    waterLevel: 'Nivel de Agua',
+    temperature: 'Temperatura'
+};
