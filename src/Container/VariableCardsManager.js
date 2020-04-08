5268fcf48156ff405ecf37fe238a6b18a7803b2c
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-08 16:23:13 -0500

    Updated from class component to functional with hooks

--- src/Container/VariableCardsManager.js
+++ src/Container/VariableCardsManager.js
@@ -1,48 +1,29 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import VariableCardsPresentation from '../Presentational/Info/VariableCards';
 import VariableAddFormManager from './VariableAddFormManager';
 import VariableConfigureFormManager from './VariableConfigureFormManager';
+import AuthContext from '../Contexts/AuthContext';
+import RiskZonesContext, { } from '../Contexts/RiskZonesContext';
 
-export default class CriticalPointCardsManager extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            showAddForm: false,
-            showConfigureForm: false,
-            variables: [1, 2, 3, 4, 5, 6, 7, ]
-        }
-    }
-    
-    handleOpenAddMenu = () => {
-        this.setState((prevState) => ({
-            showAddForm: !prevState.showAddForm
-        }));
-    }
-    
-    handleOpenConfigureMenu = () => {
-        this.setState((prevState) => ({
-            showConfigureForm: !prevState.showConfigureForm
-        }));
-    }
-    
-    render() {
-        return (
-            <div>
-                <VariableCardsPresentation
-                    variables={this.state.variables}
-                    handleOpenAddMenu={this.handleOpenAddMenu}
-                    handleOpenConfigureMenu={this.handleOpenConfigureMenu}
-                />
-                <VariableConfigureFormManager
-                    showDialog={this.state.showConfigureForm}
-                    handleClose={this.handleOpenConfigureMenu}
-                />
-                <VariableAddFormManager
-                    showAddForm={this.state.showAddForm}
-                    handleClose={this.handleOpenAddMenu}
-                />
-            </div>
-        )
-    }
+export default () => {
+    const [showingAddForm, setShowingAddForm] = useState(false);
+    const [showingConfigureForm, setShowingConfigureForm] = useState(false);
+    const variables = [1, 2, 3, 4, 5, 6, 7 ];
+    return (
+        <div>
+            <VariableCardsPresentation
+                variables={variables}
+                handleOpenAddMenu={() => (setShowingAddForm(true))}
+                handleOpenConfigureMenu={() => (setShowingConfigureForm(true))}
+            />
+            <VariableConfigureFormManager
+                showDialog={showingConfigureForm}
+                handleClose={() => (setShowingConfigureForm(false))}
+            />
+            <VariableAddFormManager
+                showAddForm={showingAddForm}
+                handleClose={() => (setShowingAddForm(false))}
+            />
+        </div>
+    )
 }
