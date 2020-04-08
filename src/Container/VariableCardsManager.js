2328d843f6263d3eaf0e052cb581780c57b47bc3
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-08 16:39:26 -0500

    Hooks imported and initialization of variables array

--- src/Container/VariableCardsManager.js
+++ src/Container/VariableCardsManager.js
@@ -1,4 +1,5 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import VariableCardsPresentation from '../Presentational/Info/VariableCards';
 import VariableAddFormManager from './VariableAddFormManager';
 import VariableConfigureFormManager from './VariableConfigureFormManager';
@@ -6,9 +7,32 @@ import AuthContext from '../Contexts/AuthContext';
 import RiskZonesContext, { } from '../Contexts/RiskZonesContext';
 
 export default () => {
+    const { riskZones } = useContext(RiskZonesContext);
+    const { token } = useContext(AuthContext);
+    const { riskZoneId, criticalSpotId, sensorNodeId } = useParams();
+    
     const [showingAddForm, setShowingAddForm] = useState(false);
     const [showingConfigureForm, setShowingConfigureForm] = useState(false);
-    const variables = [1, 2, 3, 4, 5, 6, 7 ];
+    
+    const [variables, setVariables] = useState([]);
+    
+    
+    
+    useEffect(() => {
+        const currentRiskZone = riskZones.find((riskZone) => riskZone._id === riskZoneId);
+        
+        if (currentRiskZone) {
+            const { criticalSpots } = currentRiskZone;
+            
+            const { sensorNodes } = criticalSpots.find((criticalSpot) => criticalSpot._id === criticalSpotId);
+            
+            const { variables } = sensorNodes.find((sensorNode) => sensorNode._id === sensorNodeId);
+            
+            setVariables(variables);
+        }    
+    }, [riskZoneId, criticalSpotId, sensorNodeId, riskZones]);
+    
+    
     return (
         <div>
             <VariableCardsPresentation
