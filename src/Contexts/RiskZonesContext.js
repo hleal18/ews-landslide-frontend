400642290123ae009ac9e61293837ed37c4ef393
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-07 19:36:59 -0500

    Contexts now fetches and merges sensorNodes or devices from Api

--- src/Contexts/RiskZonesContext.js
+++ src/Contexts/RiskZonesContext.js
@@ -4,25 +4,36 @@ import ewsApi from '../Api/ewsApi';
 
 const RiskZonesContext = React.createContext({
     riskZones: [],
-    setRiskZones: () => {}
+    setRiskZones: () => { }
 });
 
 export const RiskZonesProvider = (props) => {
     const [riskZones, setRiskZones] = useState([]);
-    
+
     const { token } = useContext(AuthContext);
-    
+
     useEffect(() => {
         const getRiskZones = async () => {
             const riskZones = await ewsApi.getRiskZones(token);
             const criticalSpots = await ewsApi.getCriticalSpots(token);
-            
+            const sensorNodes = await ewsApi.getSensorNodes(token);
+            console.log('Sensor Nodes on context: ', sensorNodes);
             // It modifies the original riskZone object from API to contain
             // composed objects with info about: criticalSpots->SensorNodes
             // ->Variables.
             const riskZonesAppData = riskZones.map((riskZone) => {
-                const criticalSpotsForRiskZone = criticalSpots.filter((criticalSpot) => 
-                    riskZone._id === criticalSpot.riskZoneId); 
+                const composedCriticalSpots = criticalSpots.map((criticalSpot) => {
+                    const filteredSensorNodes = sensorNodes.filter((sensorNode) => criticalSpot._id === sensorNode.criticalSpotId)
+
+                    return {
+                        ...criticalSpot,
+                        sensorNodes: filteredSensorNodes
+                    }
+                });
+                
+                const criticalSpotsForRiskZone = composedCriticalSpots.filter((criticalSpot) => 
+                    riskZone._id === criticalSpot.riskZoneId);
+                
                 const composedRiskZone = {
                     ...riskZone,
                     criticalSpots: criticalSpotsForRiskZone
@@ -33,7 +44,7 @@ export const RiskZonesProvider = (props) => {
         }
         getRiskZones();
     }, [token]);
-    
+
     return (
         <RiskZonesContext.Provider value={{ riskZones, setRiskZones }}>
             {props.children}
@@ -43,7 +54,7 @@ export const RiskZonesProvider = (props) => {
 
 export const useCriticalSpotUpdater = () => {
     const { riskZones, setRiskZones } = useContext(RiskZonesContext);
-    
+
     // Function to add a new critical spot to a specified riskZone.
     return (criticalSpot) => {
         const { riskZoneId } = criticalSpot;
