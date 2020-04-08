f400096a25f8e1a605064bcf06b8bc5c2f0dc4ed
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-08 11:28:03 -0500

    Validation of add form for device and submit function implementation

--- src/Container/DeviceAddFormManager.js
+++ src/Container/DeviceAddFormManager.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
 import DeviceAddForm from '../Presentational/Forms/DeviceAddForm';
+import ewsApi from '../Api/ewsApi';
 
 export default class DeviceAddFormManager extends React.Component {
     constructor(props) {
         super(props);
-        
+
         this.errorMessage = {
             empty: {
-                name: 'Nombre del punto crítico no debe estar vacío'
+                name: 'Nombre del dispositivo no debe estar vacío',
+                description: 'Descripción del nodo sensor no debe estar vacío'
             }
         }
-        
+
         this.state = {
             input: {
                 name: '',
                 description: ''
             },
             errorState: {
-                name: undefined
+                name: undefined,
+                description: undefined
             },
             isLoading: false,
             submitError: {
@@ -27,44 +30,65 @@ export default class DeviceAddFormManager extends React.Component {
             }
         }
     }
-    
+
     validateEmptyField = (field) => {
         if (typeof field !== 'string') throw new Error(`Field ${field} must be a string`);
         return field.length === 0 ? 'empty' : false;
     }
-    
-    validateFields = ({ name, description, lat, lng }) => {
+
+    validateFields = ({ name, description }) => {
         const results = {
             name: undefined,
             description: undefined
         }
-        
+
         let error = this.validateEmptyField(name);
-        
+
         if (error) results.name = this.errorMessage[error].name;
-        
+
+        error = this.validateEmptyField(description);
+
+        if (error) results.description = this.errorMessage[error].description;
+
         return results;
     }
-    
-    handleSubmit = () => {
+
+    handleSubmit = async () => {
         const { input } = this.state;
         const { name, description } = input;
-        
+        const { criticalSpotId, token } = this.props;
+
         const validationResults = this.validateFields({ name, description });
-        
-        this.setState({ errorState: {...validationResults} });
-        
+
+        this.setState({ errorState: { ...validationResults } });
+
         if (!validationResults.name &&
             !validationResults.description) {
-            
-            this.setState({ isLoading: true });
+
+
+            try {
+                this.setState({ isLoading: true });
+                const sensorNode = await ewsApi.addSensorNode(name, description, criticalSpotId, token);
+
+                this.setState({
+                    isLoading: false, 
+                    input: {
+                        name: '',
+                        description: ''
+                    }
+                });
+                this.props.setSensorNode(sensorNode);
+                this.props.handleClose();
+            } catch (e) {
+                console.log(e.message);
+            }
         }
     }
-    
+
     handleText = (e) => {
         const id = e.target.id;
         const value = e.target.value;
-        
+
         this.setState((prevState) => ({
             input: {
                 ...prevState.input,
@@ -72,11 +96,11 @@ export default class DeviceAddFormManager extends React.Component {
             }
         }));
     }
-    
+
     render() {
         return (
             <div>
-                <DeviceAddForm 
+                <DeviceAddForm
                     {...this.props}
                     {...this.state}
                     {...this.errorMessage}
