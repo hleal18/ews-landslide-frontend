17b2488df9c680e5c7ab04ea25f473730d46423d
Humberto Leal <humberto.adolfo@hotmail.com>
2020-04-09 15:06:05 -0500

    UseEffect interrumptions by undefined vars refactored

--- src/Container/VariableCardsManager.js
+++ src/Container/VariableCardsManager.js
@@ -11,48 +11,50 @@ export default () => {
     const { token } = useContext(AuthContext);
     const { riskZoneId, criticalSpotId, sensorNodeId } = useParams();
     const setVariable = useVariableUpdater();
-    
+
     const [showingAddForm, setShowingAddForm] = useState(false);
     const [showingConfigureForm, setShowingConfigureForm] = useState(false);
-    
+
     const [variables, setVariables] = useState([]);
     const [idSensorsTaken, setIdSensorsTaken] = useState(new Set());
     const [sensorNodeName, setSensorNodeName] = useState('');
-    
+
     const [currentRiskZone, setCurrentRiskZone] = useState(undefined);
     const [currentCriticalSpot, setCurrentCriticalSpot] = useState(undefined);
     const [currentSensorNode, setCurrentSensorNode] = useState(undefined);
-    
+
     useEffect(() => {
         setCurrentRiskZone(riskZones.find((riskZone) => riskZone._id === riskZoneId));
-        
-        if (currentRiskZone) {
-            const { criticalSpots } = currentRiskZone;
-            setCurrentCriticalSpot(criticalSpots.find((criticalSpot) => criticalSpot._id === criticalSpotId));
-            
 
-            if (currentCriticalSpot) {
-                const { sensorNodes } = currentCriticalSpot;
-                setCurrentSensorNode(sensorNodes.find((sensorNode) => sensorNode._id === sensorNodeId));
-                
-                if (currentSensorNode) {
-                    const { variables, name } = currentSensorNode;
-                    
-                    setSensorNodeName(name);
-                    setVariables(variables);
-                    setIdSensorsTaken(variables.reduce((curSet, variable) => {
-                        curSet.add(variable.idSensor);
-                        return curSet;
-                    }, new Set()))
-                }
-            }
-        }    
-    }, [riskZoneId, 
-        criticalSpotId, 
-        sensorNodeId, 
-        riskZones, 
-        currentRiskZone, 
-        currentCriticalSpot, 
+        if (!currentRiskZone) return;
+
+        const { criticalSpots } = currentRiskZone;
+        setCurrentCriticalSpot(criticalSpots.find((criticalSpot) => criticalSpot._id === criticalSpotId));
+
+
+        if (!currentCriticalSpot) return;
+
+        const { sensorNodes } = currentCriticalSpot;
+        setCurrentSensorNode(sensorNodes.find((sensorNode) => sensorNode._id === sensorNodeId));
+
+        if (!currentSensorNode) return;
+        const { variables, name } = currentSensorNode;
+
+        setSensorNodeName(name);
+        setVariables(variables);
+        setIdSensorsTaken(variables.reduce((curSet, variable) => {
+            curSet.add(variable.idSensor);
+            return curSet;
+        }, new Set()))
+
+
+
+    }, [riskZoneId,
+        criticalSpotId,
+        sensorNodeId,
+        riskZones,
+        currentRiskZone,
+        currentCriticalSpot,
         currentSensorNode]);
 
     return (
