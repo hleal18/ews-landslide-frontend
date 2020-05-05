121d11074fba2ace3aa286b95b5b50c05083035a
Humberto Leal <humberto.adolfo@hotmail.com>
2020-05-05 11:28:18 -0500

    Comments deleted

--- src/Container/DashboardManager.js
+++ src/Container/DashboardManager.js
@@ -13,8 +13,7 @@ import DashboardFilters from '../Container/DashboardFiltersBars';
 export default () => {
     const { riskZones, setRiskZones } = useContext(RiskZonesContext);
     const [selectedRiskZone, setSelectedRiskZone] = useState({});
-    console.log('RiskZones from context: ', riskZones);
-    console.log('Wenas wenas');
+    
     return (
         <div>
             <Grid container direction="column">
@@ -23,11 +22,12 @@ export default () => {
                     <Grid item xs={12}>
                         <DashboardFilters
                             riskZones={riskZones}
+                            setSelectedRiskZone={setSelectedRiskZone}
                         />
                     </Grid>
                 </Grid>
                 <Grid item xs color={grey[900]}>
-                    <Dashboard />
+                    <Dashboard selectedRiskZone={selectedRiskZone}/>
                 </Grid>
             </Grid>
         </div>
