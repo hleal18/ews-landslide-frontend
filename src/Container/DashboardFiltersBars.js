cf01dbfd6b259ccc2ab95dd763bffef02a8acc4b
Humberto Leal <humberto.adolfo@hotmail.com>
2020-05-05 11:27:56 -0500

    Filters with Select components, enable, disable and updates to parent component

--- src/Container/DashboardFiltersBars.js
+++ src/Container/DashboardFiltersBars.js
@@ -38,6 +38,9 @@ export default class DashboardFilters extends React.Component {
                 names: ['Todos'],
                 options: [''],
                 disabled: true
+            },
+            selectedRiskZone: {
+                
             }
         }
     }
@@ -77,138 +80,140 @@ export default class DashboardFilters extends React.Component {
             const criticalSpots = this.findCriticalSpots(value);
             const criticalSpotsNames = criticalSpots.map((criticalSpot) => criticalSpot.name);
             const criticalSpotsOptions = criticalSpots.map((criticalSpot) => criticalSpot._id);
+            const selectedRiskZone = {...this.props.riskZones.find((riskZone) => riskZone._id === value)};
             
-            this.setState((prev) => ({
-                riskZones: {
-                    ...prev.riskZones,
-                    value  
-                },
-                criticalSpots: {
-                    ...prev.criticalSpots,
-                    value: '',
-                    names: ['Todos', ...criticalSpotsNames],
-                    options:['', ...criticalSpotsOptions],
-                    disabled: false
-                },
-                devices: {
-                    ...prev.devices,
-                    value: '',
-                    disabled: true
-                },
-                variables: {
-                    ...prev.variables,
-                    value: '',
-                    disabled: true
-                }
-            }));
+            this.setSelection('riskZones', value);
+            this.enableAndSetOptions('criticalSpots', criticalSpotsNames, criticalSpotsOptions);
+            this.disableAndCleanSelect('devices');
+            this.disableAndCleanSelect('variables');
+            this.setSelectedRiskZone(selectedRiskZone);
         } else if (id === 'criticalSpots' && value !== '') {
             const devices = this.findSensorNodes(value);
 
             const devicesNames = devices.map((device) => device.name);
             const devicesOptions = devices.map((device) => device._id);
-
-            this.setState((prev) => ({
-                criticalSpots: {
-                    ...prev.criticalSpots,
-                    value
-                },
-                devices: {
-                    ...prev.devices,
-                    value: '',
-                    names: ['Todos', ...devicesNames],
-                    options: ['', ...devicesOptions],
-                    disabled: false
-                },
-                variables: {
-                    ...prev.variables,
-                    value: '',
-                    disabled: true
-                }
-            }));
+            const selectedRiskZone = {...this.state.selectedRiskZone};
+            const currentCriticalSpot = this.getCurrentCriticalSpot(value);
+            
+            selectedRiskZone.criticalSpots = [
+                currentCriticalSpot
+            ];
+            
+            this.setSelection('criticalSpots', value);
+            this.enableAndSetOptions('devices', devicesNames, devicesOptions);
+            this.disableAndCleanSelect('variables');
+            this.setSelectedRiskZone(selectedRiskZone);
         } else if (id === 'devices' && value !== '') {
             const variables = this.findVariables(value);
             const variablesNames = [];
             const variablesOptions = [];
-
+            const selectedRiskZone = {...this.state.selectedRiskZone};
+            const currentCriticalSpotId = selectedRiskZone.criticalSpots[0]._id;
+            const currentSensorNode = this.getCurrentSensorNode(value, currentCriticalSpotId);
+            
+            console.log('CurrentSensorNode: ', currentSensorNode, 'for value: ', value);
+            
+            selectedRiskZone.criticalSpots[0].sensorNodes = [currentSensorNode];
+            console.log('Selected riskZone: ', selectedRiskZone);
+            
             variables.forEach((variable) => {
                 variablesNames.push(variable.name);
                 variablesOptions.push(variable._id);
             });
 
-            this.setState((prev) => ({
-                devices: {
-                    ...prev.devices,
-                    value
-                },
-                variables: {
-                    ...prev.variables,
-                    value: '',
-                    names: ['Todos', ...variablesNames],
-                    options: ['', ...variablesOptions],
-                    disabled: false
-                }
-            }));
-        } else if (id === 'riskZones') {
-            this.setState((prev) => ({
-                riskZones: {
-                    ...prev.riskZones,
-                    value  
-                },
-                criticalSpots: {
-                    ...prev.criticalSpots,
-                    value,
-                    disabled: true
-                },
-                devices: {
-                    ...prev.devices,
-                    value,
-                    disabled: true
-                },
-                variables: {
-                    ...prev.variables,
-                    value,
-                    disabled: true
-                }
-            }));
+            this.setSelection('devices', value);
+            this.enableAndSetOptions('variables', variablesNames, variablesOptions);
+            this.setSelectedRiskZone(selectedRiskZone);
+        } else if (id === 'riskZones') {            
+            this.setSelection('riskZones', value);
+            this.disableAndCleanSelect('criticalSpots')
+            this.disableAndCleanSelect('devices')
+            this.disableAndCleanSelect('variables')
         } else if (id === 'criticalSpots') {
-            this.setState((prev) => ({
-                criticalSpots: {
-                    ...prev.criticalSpots,
-                    value
-                },
-                devices: {
-                    ...prev.devices,
-                    value,
-                    disabled: true
-                },
-                variables: {
-                    ...prev.variables,
-                    value,
-                    disabled: true
-                }
-            }))
+            this.setSelection('criticalSpots', value);
+            this.disableAndCleanSelect('devices')
+            this.disableAndCleanSelect('variables')
         } else if (id === 'devices') {
-            this.setState((prev) => ({
-                devices: {
-                    ...prev.devices,
-                    value
-                },
-                variables: {
-                    ...prev.variables,
-                    value,
-                    disabled: true
-                }
-            }))
+            this.setSelection('devices', value);
+            this.disableAndCleanSelect('variables')
         } else {
             this.setState((prev) => ({
                 [id]: {
                     ...prev[id],
                     value
                 }
-            }), () => console.log('Nuevo state: ', this.state));
+            }));
         }
     }
+    
+    setSelection(attributeName, value) {
+        this.setState((prev) => ({
+            [attributeName]: {
+                ...prev[attributeName],
+                value
+            }
+        }));
+    }
+    
+    setSelectedRiskZone(selectedRiskZone) {
+        this.setState((prev) => ({
+            selectedRiskZone
+        }));
+        
+        this.props.setSelectedRiskZone(selectedRiskZone);
+    }
+    
+    enableAndSetOptions(attributeName, attributeNames, attributeOptions) {
+        this.setState((prev) => ({
+            [attributeName]: {
+                ...prev[attributeName],
+                value: '',
+                names: ['Todos', ...attributeNames],
+                options: ['', ...attributeOptions],
+                disabled: false
+            }
+        }));
+    }
+    
+    disableAndCleanSelect(attributeName) {
+        this.setState((prev) => ({
+            [attributeName]: {
+                ...prev[attributeName],
+                value: '',
+                disabled: true
+            }
+        }));
+    }
 
+    getCurrentCriticalSpot(criticalSpotId) {
+        const currentRiskzone = this.props.riskZones.find((riskZone) => riskZone._id === this.state.riskZones.value);
+        
+        const currentCriticalSpot = currentRiskzone.criticalSpots.find((criticalSpot) => criticalSpot._id === criticalSpotId);
+        
+        return {...currentCriticalSpot};
+    }
+    
+    getCurrentSensorNode(sensorNodeId) {
+        // let currentSensorNode = undefined;
+        // const currentCriticalSpot = this.getCurrentCriticalSpot(criticalSpotId);
+        // console.log('CurrentCriticalSpot: ', currentCriticalSpot);
+        // currentSensorNode = currentCriticalSpot.sensorNodes.find((sensorNode) => 
+        //     sensorNode._id === sensorNodeId);
+            
+        // return currentSensorNode;
+        console.log('RiskZones: ', this.props.riskZones);
+        const currentRiskzone = this.props.riskZones.find((riskZone) =>
+            riskZone._id === this.state.riskZones.value);
+        console.log('CurrentRiskZone: ', currentRiskzone);
+        const currentCriticalSpot = currentRiskzone.criticalSpots.find((criticalSpot) =>
+            criticalSpot._id === this.state.criticalSpots.value);
+        console.log('Current CriticalSpot: ', currentCriticalSpot);
+        const currentSensorNode = currentCriticalSpot.sensorNodes.find((sensorNode) =>
+            sensorNode._id === sensorNodeId);
+        console.log('Current sensorNode: ', currentSensorNode);
+        return currentSensorNode;        
+    }
+    
     findCriticalSpots(riskZoneId) {
         const { riskZones } = this.props;
         const criticalSpots = [];
@@ -217,7 +222,7 @@ export default class DashboardFilters extends React.Component {
             if (riskZone._id !== riskZoneId) return false;
             
             riskZone.criticalSpots.forEach((criticalSpot) => 
-                criticalSpots.push(criticalSpot));
+                criticalSpots.push({...criticalSpot}));
             return true;
         });
 
@@ -230,7 +235,7 @@ export default class DashboardFilters extends React.Component {
         riskZones.find((riskZone) => {
             const criticalSpot = riskZone.criticalSpots.find((criticalSpot) => criticalSpot._id === criticalSpotId);
             if (!criticalSpot) return false;
-            criticalSpot.sensorNodes.forEach((sensorNode) => sensorNodes.push(sensorNode));
+            criticalSpot.sensorNodes.forEach((sensorNode) => sensorNodes.push({...sensorNode}));
             return true;
         });
         return sensorNodes;
@@ -245,7 +250,7 @@ export default class DashboardFilters extends React.Component {
                 const sensorNode = criticalSpot.sensorNodes.find((sensorNode) => sensorNode._id === sensorNodeId);
                 if (!sensorNode) return false;
 
-                sensorNode.variables.forEach((variable) => variables.push(variable));
+                sensorNode.variables.forEach((variable) => variables.push({...variable}));
                 return true;
             });
             
@@ -266,9 +271,6 @@ export default class DashboardFilters extends React.Component {
                 variables
             } 
         } = this;
-                
-
-        console.log('Classes: ', classes);
 
         return (
             <div >
