975521b80e81d372c2d22f538dca1fd1e1d6265a
Humberto <humberto.adolfo@hotmail.com>
2020-11-21 15:02:58 -0500

    Map asking for leogocation and centering map around that

--- src/Presentational/Info/DeviceCard.js
+++ src/Presentational/Info/DeviceCard.js
@@ -1,77 +1,146 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Chip from '@material-ui/core/Chip';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Chip from "@material-ui/core/Chip";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+import { variablesDefinitionAsObject } from "../../lib/variablesDefinition";
 
 const useStyles = makeStyles({
-    root: {
-        minWidth: 275,
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-        fontSize: 12
-    },
-    chip: {
-        marginBottom: 4
-    },
-    secondTitle: {
-        marginBottom: 8
-    }
+  root: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+    fontSize: 12,
+  },
+  chip: {
+    marginBottom: 4,
+  },
+  secondTitle: {
+    marginBottom: 8,
+  },
 });
 
-export default function DeviceCard({ name, description, redirect }) {
-    const classes = useStyles();
+export default function DeviceCard({ name, description, redirect, variables }) {
+  const classes = useStyles();
+  const variablesMetadata = new Set(
+    variables.map((variable) => variablesDefinitionAsObject[variable.type])
+  );
 
-    return (
-        <Card className={classes.root}>
-            <CardContent>
-                <Typography className={classes.title} color="textSecondary" >
-                    Nodo sensor
+  const variablesMetadataArr = [];
+
+  variablesMetadata.forEach((varType) => variablesMetadataArr.push(varType));
+  console.log("variablesmetadatasize", variablesMetadata.size);
+
+  const thresholdsMetadata = new Set(
+    variables
+      .filter((variable) => variable.threshold !== undefined)
+      .map((variable) => variablesDefinitionAsObject[variable.type])
+  );
+
+  const thresholdsMetadataArr = [];
+
+  console.log("variables:", variables);
+
+  thresholdsMetadata.forEach((varType) => thresholdsMetadataArr.push(varType));
+  console.log("Sizethesholdsmetadataset", thresholdsMetadata.size);
+
+  console.log("araray", thresholdsMetadataArr);
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary">
+          Nodo sensor
+        </Typography>
+        <Typography
+          variant="h5"
+          component="h2"
+          className={classes.secondTitle}
+          color="primary"
+        >
+          {name}
+        </Typography>
+        <Grid container spacing={0} direction="column" className={classes.pos}>
+          {thresholdsMetadataArr.length === 0 ? (
+            <Grid item>
+              <Typography className={classes.pos} color="textSecondary">
+                No hay umbrales configurados aun.
+              </Typography>
+            </Grid>
+          ) : (
+            <Grid container direction="row">
+              <Grid item xs={3}>
+                <Typography className={classes.pos} color="textSecondary">
+                  Umbrales:
                 </Typography>
-                <Typography variant="h5" component="h2" className={classes.secondTitle} color='primary'>
-                    {name}
-                </Typography>
-                <Grid container spacing={0} direction="column" className={classes.pos}>
-                    <Grid container direction="row">
-                        <Grid item xs={3}>
-                            <Typography className={classes.pos} color="textSecondary">
-                                Umbrales:
-                        </Typography>
-                        </Grid>
-                        <Grid container item xs spacing={1} className={classes.chip} direction="column">
-                            <Grid item xs><Chip label="Humedad de suelo x1" size="small" /></Grid>
-                            <Grid item xs> <Chip label="Inclinación x2" size="small" /></Grid>
-                        </Grid>
-                    </Grid>
-                    <Grid container direction="row">
-                        <Grid item xs={3} >
-                            <Typography className={classes.pos} color="textSecondary">
-                                Variables:
-                        </Typography>
-                        </Grid>
-                        <Grid container item xs spacing={1} direction="column">
-                            <Grid item xs><Chip label="Humedad de suelo" size="small" /></Grid>
-                            <Grid item xs> <Chip label="Inclinación" size="small" /></Grid>
-                        </Grid>
-                    </Grid>
+              </Grid>
+              <Grid
+                container
+                item
+                xs
+                spacing={1}
+                className={classes.chip}
+                direction="column"
+              >
+                {thresholdsMetadataArr.map((varType) => (
+                  <Grid item xs>
+                    <Chip label={varType} size="small" />
+                  </Grid>
+                ))}
+                <Grid item xs>
+                  <Chip label="Humedad de suelo x1" size="small" />
                 </Grid>
-                <Typography variant="body2" component="p">
-                    {description ? description : <i>Sin descripción</i>}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Grid container spacing={2} justify="center" direction="row">
-                    <Grid item><Button size="small" color="primary" onClick={redirect}>Ver Variables</Button></Grid>
+                <Grid item xs>
+                  {" "}
+                  <Chip label="Inclinación x2" size="small" />
                 </Grid>
-            </CardActions>
-        </Card>
-    );
+              </Grid>
+            </Grid>
+          )}
+          {variablesMetadataArr.length === 0 ? (
+            <Grid item>
+              <Typography className={classes.pos} color="textSecondary">
+                No hay variables monitoreadas aun.
+              </Typography>
+            </Grid>
+          ) : (
+            <Grid container direction="row">
+              <Grid item xs={3}>
+                <Typography className={classes.pos} color="textSecondary">
+                  Variables:
+                </Typography>
+              </Grid>
+              <Grid container item xs spacing={1} direction="column">
+                {variablesMetadataArr.map((varType) => (
+                  <Grid item xs>
+                    <Chip label={varType} size="small" />
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          )}
+        </Grid>
+        <Typography variant="body2" component="p">
+          {description ? description : <i>Sin descripción</i>}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Grid container spacing={2} justify="center" direction="row">
+          <Grid item>
+            <Button size="small" color="primary" onClick={redirect}>
+              Ver Variables
+            </Button>
+          </Grid>
+        </Grid>
+      </CardActions>
+    </Card>
+  );
 }
--- src/Presentational/Info/Map/Map.js
+++ src/Presentational/Info/Map/Map.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker'
 
@@ -63,26 +63,28 @@ export default function SimpleMap({
     position,
     handleClick
  }) {
-    
-    const center = {
-        lat: 10.399,
-        lng: -75.51
-    };
-    
-    const zoom = 11;
+    const defaultCenter = { lat: 10.399, lng: -75.51 };
+    const [center, setCenter] = useState({ lat: 10.399, lng: -75.51 });
+    navigator.geolocation.getCurrentPosition((position) => {
+        // console.log('current position', position);
+        setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
+    });
+
+    const zoom = 15;
     
     return (
         <div style={{ height: '100vh', width: '100%' }}>
             <GoogleMapReact
                 bootstrapURLKeys={{ key: 'AIzaSyANwB6gQaXT2StJWRgbR6DV4tA5cBDm2PE' }}
-                defaultCenter={center}
+                defaultCenter={defaultCenter}
+                center={center}
                 defaultZoom={zoom}
                 onClick={handleClick}
             >
                 <Marker 
                   lat={position.lat}
                   lng={position.lng}
-                  text="My Marker"
+                  text="Punto critico"
                 />
             </GoogleMapReact>
         </div>
