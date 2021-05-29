7fbb0c907718d16c29d9db384d3e28fa4de9da8f
Humberto <humberto.adolfo@hotmail.com>
2021-05-29 16:55:17 -0500

    form submission, validation, implementation of custom error and loading state

--- src/Container/UserManager.js
+++ src/Container/UserManager.js
@@ -9,9 +9,11 @@ import Chip from "@material-ui/core/Chip";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 import UserContext from "../Contexts/UserContext";
 import AuthContext from "../Contexts/AuthContext";
+import ewsApi from "../Api/ewsApi";
 
 const useStyles = makeStyles({
     root: {
@@ -35,25 +37,99 @@ const useStyles = makeStyles({
     },
 });
 
+class CustomError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "CustomError";
+    }
+}
+
+const validateEmail = (email) => {
+    if (typeof email !== 'string') throw new Error(`Field ${email} must be a string`);
+    if (!email.match(/[^@]+@[^\.]+\..+/g)) 
+        throw new CustomError("Formato invalido para email");
+    else return false;
+}
+
+const validateUserSubmit = ({
+    firstName,
+    lastName,
+    email,
+    emailsToNotify,
+}) => {
+    if (!firstName || firstName.length === 0)
+        throw new CustomError(`Primer nombre no puede estar vacio`);
+    if (!lastName || lastName.length === 0)
+        throw new CustomError(`Apellido no puede estar vacio`);
+    if (!email || email.length === 0) {
+        validateEmail(email);
+        throw new CustomError(`Correo no puede estar vacio`);
+    }
+    if (!emailsToNotify || emailsToNotify.length === 0)
+        throw new CustomError(`Correos a notificar no pueden estar vacios`);
+};
+
+const adaptEmailsToNotifyInput = (emailsToNotifyStr) => {
+    try {
+        const result = emailsToNotifyStr.split(",").map((str) => str.trim());
+        console.log("result", result);
+        result.forEach((email) => validateEmail(email));
+        return result;
+    } catch (e) {
+        throw new CustomError(
+            "Error con los correos a notificar, verifique que esten separados por coma y este en el formato apropiado"
+        );
+    }
+};
 export const UserManager = () => {
     const classes = useStyles();
-    const { user } = useContext(UserContext);
+    const { user, setUser } = useContext(UserContext);
     const { token } = useContext(AuthContext);
     const [editModeEnabled, setEditModeEnabled] = useState(false);
     const [firstName, setFirstName] = useState(user.firstName ?? "");
     const [lastName, setLastName] = useState(user.lastName ?? "");
-    const [email, setEmail] = useState(user.email ?? '');
+    const [email, setEmail] = useState(user.email ?? "");
     const [emailsToNotify, setEmailsToNotify] = useState(
-        user.emailsToNotify?.join(',\t') ?? ''
+        user.emailsToNotify?.join(",\t") ?? ""
     );
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(undefined);
 
     console.log("EmailsToNotify", emailsToNotify);
     useMemo(() => {
         setFirstName(user.firstName ?? "");
         setLastName(user.lastName ?? "");
         setEmail(user.email ?? "");
-        setEmailsToNotify(user.emailsToNotify?.join(',\t') ?? '');
+        setEmailsToNotify(user.emailsToNotify?.join(",\t") ?? "");
     }, [user]);
+
+    const handleSubmit = async () => {
+        setIsLoading(true);
+        try {
+            validateUserSubmit({ firstName, lastName, email, emailsToNotify });
+            const adaptedEmailsToNotify =
+                adaptEmailsToNotifyInput(emailsToNotify);
+
+            const updatedUser = await ewsApi.patchUser(token, {
+                firstName,
+                lastName,
+                email,
+                emailsToNotify: adaptedEmailsToNotify,
+            });
+            setUser(updatedUser);
+            setEditModeEnabled(false);
+            setError(undefined);
+        } catch (e) {
+            console.log('error: ', e.message);
+            if (e.name === "CustomError") {
+                setError(e.message);
+            } else {
+                setError(`Sucedio un error ${e.message}`);
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     console.log("User information: ", user);
     return (
@@ -121,12 +197,13 @@ export const UserManager = () => {
                                 autoFocus
                                 margin="dense"
                                 id="firstName"
-                                disabled={true}
+                                disabled={!editModeEnabled}
+                                onChange={(e) => setEmail(e.target.value)}
                                 // label="Primer Nombre"
                                 fullWidth
                                 required={true}
                                 value={email}
-                                helperText='Correo usado durante registro no se puede modificar'
+                                // helperText="Correo usado durante registro no se puede modificar"
                                 // onChange={(e) => setFirstName(e.target.value)}
                                 // onChange={props.handleText}
                                 // helperText={props.errorState.name}
@@ -160,6 +237,17 @@ export const UserManager = () => {
                             />
                         </FormWithLabel>
                     </Typography>
+                    {
+                        error && 
+                        <Typography
+                            variant="body2"
+                            component="p"
+                            className={classes.textField}
+                            color='error'
+                        >
+                            {error}
+                        </Typography>
+                    }   
                 </CardContent>
                 <CardActions>
                     <Grid
@@ -191,11 +279,24 @@ export const UserManager = () => {
                                 <Button
                                     size="small"
                                     color="primary"
-                                    onClick={() => setEditModeEnabled(true)}
+                                    onClick={() => handleSubmit()}
                                     className={classes.watch_node_button}
                                 >
                                     Actualizar datos
                                 </Button>
+                                {isLoading && (
+                                    <Grid
+                                        container
+                                        justify="center"
+                                        spacing={2}
+                                    >
+                                        <Grid item>
+                                            <div>
+                                                <CircularProgress />
+                                            </div>
+                                        </Grid>
+                                    </Grid>
+                                )}
                             </div>
                         )}
                     </Grid>
