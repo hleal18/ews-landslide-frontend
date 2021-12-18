676a427973201ba3fb3b744ac92c672358502dbd
Humberto Leal <humberto.adolfo@hotmail.com>
2021-12-18 17:43:12 -0500

    improved risk zone cards to show metadata about notificationsEnabled field on a text

--- src/Presentational/Info/RiskZoneCard.js
+++ src/Presentational/Info/RiskZoneCard.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Chip from '@material-ui/core/Chip';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Chip from "@material-ui/core/Chip";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Switch from "@material-ui/core/Switch";
+
 
 const useStyles = makeStyles({
     root: {
@@ -17,32 +19,74 @@ const useStyles = makeStyles({
     },
     pos: {
         marginBottom: 12,
-        fontSize: 12
+        fontSize: 12,
     },
     watch_node_button: {
-        marginRight: 25
-    }
+        marginRight: 25,
+    },
 });
 
-export default function RiskZoneCard({ redirect, name, description }) {
+export default function RiskZoneCard({ config, redirect, name, description, notificationsEnabled = false }) {
     const classes = useStyles();
-    
+
     return (
         <Card className={classes.root}>
             <CardContent>
-                <Typography className={classes.title} color="textSecondary" >
+                <Typography className={classes.title} color="textSecondary">
                     Zona de riesgo
                 </Typography>
-                <Typography variant="h5" component="h2" color='primary'>
+                <Typography
+                    variant="h5"
+                    component="h2"
+                    color="primary"
+                    style={{ marginBottom: 6 }}
+                >
                     {name}
                 </Typography>
+                <Typography
+                    variant="body2"
+                    component="p"
+                    style={{ marginBottom: 6 }}
+                >
+                    {description ? (
+                        description
+                    ) : (
+                        <i>Sin descripción especificada</i>
+                    )}
+                </Typography>
                 <Typography variant="body2" component="p">
-                    {description ? description : <i>Sin descripción especificada</i>}
+                    Notificaciones:{" "}
+                    <b>
+                        <i>
+                            {notificationsEnabled
+                                ? "Activadas"
+                                : "Desactivadas"}
+                        </i>
+                    </b>
                 </Typography>
             </CardContent>
             <CardActions>
-                <Grid container item spacing={1} justify="center" direction="row">
-                    <Button size="small" color="primary" onClick={redirect} className={classes.watch_node_button}>Ver Puntos Críticos</Button>
+                <Grid container justify="center" direction="row">
+                    <Grid item spacing={1} justify="center" direction="column">
+                        <Button
+                            size="small"
+                            color="primary"
+                            onClick={redirect}
+                            className={classes.watch_node_button}
+                        >
+                            Ver Puntos Críticos
+                        </Button>
+                    </Grid>
+                    <Grid item spacing={1} justify="center" direction="column">
+                        <Button
+                            size="small"
+                            color="primary"
+                            onClick={config}
+                            className={classes.watch_node_button}
+                        >
+                            Notificaciones
+                        </Button>
+                    </Grid>
                 </Grid>
             </CardActions>
         </Card>
--- src/Presentational/Info/RiskZoneCards.js
+++ src/Presentational/Info/RiskZoneCards.js
@@ -4,43 +4,69 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import RiskZoneCard from "./RiskZoneCard";
-import BreadCrumbs from './BreadCrumbs';
-import { withRouter } from 'react-router-dom';
-import AddButton from '../AddButton'
+import BreadCrumbs from "./BreadCrumbs";
+import { withRouter } from "react-router-dom";
+import AddButton from "../AddButton";
 
 const InfoComponent = (props) => (
-    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} >
-        <RiskZoneCard {...props} redirect={() => props.history.push(props.route)} />
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+        <RiskZoneCard
+            {...props}
+            redirect={() => props.history.push(props.route)}
+            // config={() => props.history.push(props.config)}
+        />
     </Grid>
 );
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
     root: {
-        marginTop: 48
-    }
+        marginTop: 48,
+    },
 }));
 
-const RiskZoneCards = ({ riskZones, history, handleOpenAddMenu }) => {
+const RiskZoneCards = ({ riskZones, history, handleOpenAddMenu, handleOpenConfigMenu }) => {
     const classes = useStyles();
-    const { location:  { pathname: currentLocation } } = history;
-    
+    const {
+        location: { pathname: currentLocation },
+    } = history;
+
     return (
         <div className={classes.root}>
             <Container maxWidth={false}>
-                <Grid container spacing={3} direction="column" >
-                    <Grid container spacing={3} direction="row" justify="flex-start">
-                        {
-                            riskZones.map((riskZone, ind) =>
-                                <InfoComponent key={ind} history={history} {...riskZone} route={`${currentLocation}/${riskZone._id}/criticalspots`}/>)
-                        }
-                        <Grid container item xs={12} sm={6} md={4} lg={3} xl={2} alignItems="center" justify="center">
+                <Grid container spacing={3} direction="column">
+                    <Grid
+                        container
+                        spacing={3}
+                        direction="row"
+                        justify="flex-start"
+                    >
+                        {riskZones.map((riskZone, ind) => (
+                            <InfoComponent
+                                key={ind}
+                                history={history}
+                                {...riskZone}
+                                route={`${currentLocation}/${riskZone._id}/criticalspots`}
+                                config={() => handleOpenConfigMenu(riskZone)}
+                            />
+                        ))}
+                        <Grid
+                            container
+                            item
+                            xs={12}
+                            sm={6}
+                            md={4}
+                            lg={3}
+                            xl={2}
+                            alignItems="center"
+                            justify="center"
+                        >
                             <AddButton handleClick={handleOpenAddMenu} />
                         </Grid>
                     </Grid>
                 </Grid>
             </Container>
         </div>
-    )
-}
+    );
+};
 
 export default withRouter(RiskZoneCards);
